2cc293ed2ce5b174a5680f83588702af92a88201
miketheofan <miketheofan@gmail.com>
2025-03-06 22:58:11 +0200

    Finished implementation for producer.

---
+++ reviews-producer.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import json
+import asyncio
+import random
+
+from aiokafka import AIOKafkaProducer
+
+from faker import Faker
+
+topic = 'test'
+
+def serializer(value):
+    return json.dumps(value).encode()
+
+def init_names():
+    names = []
+
+    for _ in range(10):
+        names.append(fake.name())
+    names.append('Michail Theofanopoulos')
+
+    return names
+
+async def produce(names, movies):
+    
+    producer = AIOKafkaProducer(
+        bootstrap_servers='localhost:29092',
+        value_serializer=serializer,
+        compression_type="gzip")
+    
+    try:
+      while True:
+        await producer.start()
+
+        name = random.choice(names)
+        movie = random.choice(movies)
+        date = fake.date_time_this_decade().strftime('%Y-%m-%d %H:%M:%S')
+        rating = random.randint(1, 10)
+
+        data = {"name": name, "movie": movie, "current_time": date, "rating": rating}
+        await producer.send(topic, data)
+        print(f"Sent: {data}")
+        
+        await asyncio.sleep(10)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Close the producer when done
+        await producer.stop()
+
+# Create a Faker instance
+fake = Faker()
+
+movies_df = pd.read_csv('./data/movies.csv')
+movies = movies_df.iloc[:, 0].tolist()
+names = init_names()
+
+loop = asyncio.get_event_loop()
+result = loop.run_until_complete(produce(names, movies))
