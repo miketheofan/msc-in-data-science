b75744c7f5b359f7b1fd63915b6565fb67ad8987
miketheofan <miketheofan@gmail.com>
2025-03-09 12:30:15 +0200

    Added some comments

--- reviews-consumer.py
+++ reviews-consumer.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StructField,StringType,IntegerType, TimestampType
+from pyspark.sql.types import StructType,StructField,StringType,IntegerType
 from pyspark.sql.functions import from_json,col, to_timestamp, date_format
 
 spark = SparkSession \
--- reviews-producer.py
+++ reviews-producer.py
@@ -16,6 +16,7 @@ def init_names(no_names=10):
 
     for _ in range(no_names):
         names.append(fake.name())
+        
     names.append('Michail Theofanopoulos')
 
     return names
@@ -32,6 +33,7 @@ async def produce(names, movies, processing_interval=30):
       while True:
         await producer.start()
 
+        # Produce the random data
         name = random.choice(names)
         movie = random.choice(movies)
         date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -41,6 +43,7 @@ async def produce(names, movies, processing_interval=30):
         await producer.send(topic, data)
         print(f"Sent: {data}")
         
+        # Sleep for certain seconds until the next data entry is sent
         await asyncio.sleep(processing_interval)
     except KeyboardInterrupt:
         pass
@@ -51,12 +54,14 @@ async def produce(names, movies, processing_interval=30):
 # Create a Faker instance
 fake = Faker()
 
-no_names = 50
+no_names = 15
 processing_interval = 50
 
+# Read the movies and initialzie the random names
 movies_df = pd.read_csv('./data/movies.csv')
 movies = movies_df.iloc[:, 0].tolist()
 names = init_names(no_names)
 
+# Run the producer indefinitely
 loop = asyncio.get_event_loop()
 result = loop.run_until_complete(produce(names, movies, processing_interval))
